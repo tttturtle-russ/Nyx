00c65163f0e85b70bfa2673616138bc08dc92e8e
TurtleRuss <tttturtleruss@gmail.com>
2024-06-09 02:24:10 +0800

    add sections and functions

--- internel/parser/disassembler.go
+++ internel/parser/disassembler.go
@@ -2,30 +2,44 @@ package parser
 
 import (
 	"github.com/radareorg/r2pipe-go"
+	"strings"
 )
 
 type Disassembler struct {
-	pipe *r2pipe.Pipe
+	pipe      *r2pipe.Pipe
+	sections  []string
+	functions []string
 }
 
-func NewDisassembler(file string) *Disassembler {
+func NewDisassembler(file string, lazyLoad bool) *Disassembler {
+	newDisassembler := &Disassembler{}
 	pipe, err := r2pipe.NewPipe(file)
 	if err != nil {
 		panic(err)
 	}
-	_, err = pipe.Cmd("aaaa")
-	if err != nil {
-		panic(err)
+	if !lazyLoad {
+		_, err = pipe.Cmd("aaaa")
+		if err != nil {
+			panic(err)
+		}
+		sections, err := pipe.Cmd("iS")
+		if err != nil {
+			panic(err)
+		}
+		newDisassembler.sections = strings.Split(sections, "\n")
+		functions, err := pipe.Cmd("afl")
+		if err != nil {
+			panic(err)
+		}
+		newDisassembler.functions = strings.Split(functions, "\n")
 	}
-	return &Disassembler{
-		pipe: pipe,
-	}
+	return newDisassembler
 }
 
-func (d *Disassembler) Disassemble(segment string) {
-	d.pipe.Cmd("s " + segment)
+func (d *Disassembler) Functions() []string {
+	return d.functions
 }
 
-func (d *Disassembler) DisassembleText() {
-
+func (d *Disassembler) Sections() []string {
+	return d.sections
 }
